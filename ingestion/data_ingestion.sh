e5d9fd618fca72d34828cac9cac09f35ce91620a
Shubham Jha <jha1shubham@gmail.com>
2022-12-19 21:21:42 -0500

    Add all files

---
+++ ingestion/data_ingestion.sh
@@ -0,0 +1,2 @@
+set -euo pipefail
+hdfs dfs -put ./data bdad_project/
---
+++ profiling/CountRecordsMapper.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+public class CountRecordsMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException{
+        context.write(new Text("records"), new IntWritable(1));
+    }
+}
